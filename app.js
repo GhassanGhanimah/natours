0155e774080d2a15ea830a9f59a46a8e7452624e
ghassanGhanimah <ghassanghanimah@yahoo.com>
2021-03-06 21:16:12 +0200

    added a comment

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
-
+//startaspfiadgpi
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
